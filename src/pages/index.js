2584058aa84c4631fc3912238fe194ab17e57758
gvorbeck <gvorbeck@gmail.com>
2019-10-21 15:32:54 -0400

    learning gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,16 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import "./index.scss"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h2>Site Coming Soon</h2>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      {/*<Image />*/}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    {/*<Link to="/page-2/">Go to page 2</Link>*/}
   </Layout>
 )
 
