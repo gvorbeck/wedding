578d23fc3d66e33c52704ffa3ad58211986151e0
jgvorbec <jgvorbec@us.ibm.com>
2020-01-07 14:07:10 -0500

    new styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,8 @@ import indexStyles from "../styles/index.module.scss"
 export default ({ data }) => (
     <Layout pageName={indexStyles.home}>
         <SEO title="Home" />
-        <section className={indexStyles.splash}>
-            <h2>{data.site.siteMetadata.title}</h2>
+        <section className={[indexStyles.splash, indexStyles.box].join(' ')}>
+            <h2 className={indexStyles.title}>{data.site.siteMetadata.title}</h2>
             <div className={indexStyles.img}>
                 <Img fluid={data.imgHands.childImageSharp.fluid}/>
             </div>
@@ -20,14 +20,14 @@ export default ({ data }) => (
             <span>We're getting married</span>
             <h3>{data.site.siteMetadata.dateNumbers}</h3>
         </section>
-        <section className={indexStyles.info}>
+        <section className={[indexStyles.info, indexStyles.box].join(' ')}>
             <div className={indexStyles.img}>
-                <Img fluid={data.imgFire.childImageSharp.fluid} />
+                <Img fluid={data.imgPire.childImageSharp.fluid}/>
             </div>
-            <p>
+            <h2 className={indexStyles.title}>
                 <span>Unionville Vineyards</span>
                 <span>New Jersey</span>
-            </p>
+            </h2>
         </section>
     </Layout>
 )
@@ -40,23 +40,16 @@ query {
             dateNumbers
         }
     },
-    imgHeads: file(relativePath: { eq: "wedding-couple.png" }) {
+    imgHands: file(relativePath: { eq: "hands_lo.png" }) {
         childImageSharp {
-            fluid(maxWidth: 800) {
+            fluid(maxWidth: 600) {
                 ...GatsbyImageSharpFluid
             }
         }
     },
-    imgHands: file(relativePath: { eq: "hands.jpg" }) {
+    imgPire: file(relativePath: { eq: "wedding-dress_lo.png" }) {
         childImageSharp {
-            fluid(maxWidth: 800) {
-                ...GatsbyImageSharpFluid
-            }
-        }
-    },
-    imgFire: file(relativePath: { eq: "wedding-dress.jpg" }) {
-        childImageSharp {
-            fluid(maxWidth: 500) {
+            fluid(maxHeight: 600) {
                 ...GatsbyImageSharpFluid
             }
         }
