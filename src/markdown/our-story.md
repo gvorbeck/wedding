b7291090f17e237792c50e0540100fe0a9d9d1d3
gvorbeck <gvorbeck@gmail.com>
2020-01-07 18:07:45 -0500

    our story mobile

--- src/markdown/our-story.md
+++ src/markdown/our-story.md
@@ -2,10 +2,10 @@
 title: "Our Story"
 ---
 
-Hi I'm Garrett, and holy shit, you're in a punk band?
+### Hi I'm Garrett, and holy shit, you're in a punk band? ###
 
 It was the day after Krishanti's 28th birthday and I was scrolling through OKCupid. Hungover from a night spent with friends at Baby Grand, Krishanti received my brilliant ice-breaker and decided to give me a chance. We chatted online about music for a bit before deciding to meet for our first date on August 3rd, 2017 at a quiet bar named Burp Castle.
 
-Yeah, obviously!
+### Yeah, obviously! ###
 
 On September 28th, 2019--the last warm weekend of the year--I asked Krishanti to go on a short walk with me to the park at the Brooklyn Piers. We found a grassy lone area where I asked Krishanti to marry me. "Yeah, obviously," she said, beaming. Afterward, a surprise engagement party awaited her in a small pizza shop nearby where we were surrounded by our friends and family.
--- src/pages/our-story.js
+++ src/pages/our-story.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,28 +9,40 @@ import storyStyles from "../styles/our-story.module.scss"
 
 export default ({ data }) => (
     <Layout pageName={storyStyles.ourStory}>
-        <SEO title="Our Story" />
-        {data.allMarkdownRemark.edges.map(({ node, i }) => (
-            <article key={node.id} className={storyStyles.story}>
+    <SEO title="Our Story" />
+    {data.allMarkdownRemark.edges.map(({ node, i }) => (
+        <article key={node.id} className={storyStyles.story}>
+            <header>
                 <h1 className={[globalStyles.title, storyStyles.title].join(' ')}>{node.frontmatter.title}</h1>
-                <section className={[globalStyles.pageContent, storyStyles.pageContent].join(' ')} dangerouslySetInnerHTML={{ __html: node.html }} />
-            </article>
-        ))}
+                <div className={storyStyles.img}>
+                    <Img fluid={data.imgCouple.childImageSharp.fluid}/>
+                </div>
+            </header>
+            <section className={[globalStyles.pageContent, storyStyles.pageContent].join(' ')} dangerouslySetInnerHTML={{ __html: node.html }} />
+        </article>
+    ))}
     </Layout>
 )
 
 export const query = graphql`
-    {
-      allMarkdownRemark(filter: {frontmatter: {title: {eq: "Our Story"}}}) {
+{
+    allMarkdownRemark(filter: {frontmatter: {title: {eq: "Our Story"}}}) {
         edges {
-          node {
-            frontmatter {
-              title
+            node {
+                frontmatter {
+                    title
+                }
+                html
+                id
             }
-            html
-            id
-          }
         }
-      }
-    }
+    },
+    imgCouple: file(relativePath: { eq: "wedding-couple_lo.png" }) {
+        childImageSharp {
+            fluid(maxHeight: 600) {
+                ...GatsbyImageSharpFluid
+            }
+        }
+    },
+}
 `
